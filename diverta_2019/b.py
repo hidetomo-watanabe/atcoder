0111772338529ed315f64884e65573c591229cb3
hidetomo-watanabe <attyamunn@gmail.com>
2019-05-14 18:03:39 +0900

    add diverta a b

---
+++ diverta_2019/b.py
@@ -0,0 +1,8 @@
+R, G, B, N = list(map(int, input().split()))
+
+ans = 0
+for r in range(N//R+1):
+    for g in range((N-r*R)//G+1):
+        if (N-r*R-g*G) % B == 0:
+            ans += 1
+print(ans)
