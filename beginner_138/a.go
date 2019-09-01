624856267345841fb741bcfe1cb0851e388b62eb
hidetomo-watanabe <attyamunn@gmail.com>
2019-09-01 12:24:54 +0000

    fix beginner_138 remove tab

--- beginner_138/a.go
+++ beginner_138/a.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-	"fmt"
+    "fmt"
 )
 
 func main() {
-	var a int64
+    var a int64
     var s string
-	fmt.Scan(&a)
-	fmt.Scan(&s)
+    fmt.Scan(&a)
+    fmt.Scan(&s)
     if a >= 3200 {
-	    fmt.Println(s)
+        fmt.Println(s)
     } else {
-	    fmt.Println("red")
+        fmt.Println("red")
     }
 }
