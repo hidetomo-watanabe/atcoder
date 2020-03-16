dae2d651301ceb40018d8ada5d00208b7ca543cb
hidetomo-watanabe <attyamunn@gmail.com>
2020-03-16 23:09:34 +0900

    add beginner_158 a

---
+++ beginner_158/a.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    if (s == "AAA" or s == "BBB") {
+        cout << "No";
+    } else {
+        cout << "Yes";
+    }
+
+    return 0;
+}
