6b72b229319b0ee5de46f325f79dd79a1f4dbb57
hidetomo-watanabe <attyamunn@gmail.com>
2021-07-19 16:25:19 +0900

    add beginner_210 a

---
+++ beginner_210/a.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+    . "fmt"
+    "os"
+    "bufio"
+    "strconv"
+)
+
+var (
+	sc = bufio.NewScanner(os.Stdin)
+	out  = bufio.NewWriter(os.Stdout)
+)
+
+func next() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	x, _ := strconv.Atoi(next())
+	return x
+}
+
+func chars(s string) []string {
+	ret := make([]string, len([]rune(s)))
+	for i, v := range []rune(s) {
+		ret[i] = string(v)
+	}
+	return ret
+}
+
+func main() {
+    // init
+	buf := make([]byte, 1024*1024)
+	sc.Buffer(buf, bufio.MaxScanTokenSize)
+	sc.Split(bufio.ScanWords)
+	defer out.Flush() // !!!!caution!!!! you must use Fprint(out, ) not Print()
+
+    var (
+        N int
+        A int
+        X int
+        Y int
+        ans int
+    )
+    N = nextInt()
+    A = nextInt()
+    X = nextInt()
+    Y = nextInt()
+
+    if N > A {
+        ans = X * A + Y * (N - A)
+    } else {
+        ans = X * N
+    }
+    Fprintln(out, ans)
+}
