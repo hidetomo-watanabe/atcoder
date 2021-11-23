fde9662334586b580e94473ce06348d0bd06eb7e
hidetomo-watanabe <attyamunn@gmail.com>
2021-11-23 15:39:43 +0900

    add beginner_226 a

---
+++ beginner_226/a.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+    . "fmt"
+    "os"
+    "bufio"
+    "strconv"
+    "math"
+)
+
+var (
+	sc = bufio.NewScanner(os.Stdin)
+	out  = bufio.NewWriter(os.Stdout)
+)
+
+func next() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	x, _ := strconv.Atoi(next())
+	return x
+}
+
+func nextFloat() float64 {
+	x, _ := strconv.ParseFloat(next(), 64)
+	return x
+}
+
+func chars(s string) []string {
+	ret := make([]string, len([]rune(s)))
+	for i, v := range []rune(s) {
+		ret[i] = string(v)
+	}
+	return ret
+}
+
+func main() {
+    // init
+	buf := make([]byte, 1024*1024)
+	sc.Buffer(buf, bufio.MaxScanTokenSize)
+	sc.Split(bufio.ScanWords)
+	defer out.Flush() // !!!!caution!!!! you must use Fprint(out, ) not Print()
+
+    var (
+        X float64
+    )
+    X = nextFloat()
+
+    Fprintln(out, math.Round(X))
+}
